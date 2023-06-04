19984fde45f3bf6160cbb39ccf599f5b5bc4aff0
greymoon <aljog786@gmail.com>
2023-06-05 05:14:57 +0530

    with footer not completed

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -36,3 +36,32 @@ hamBurger.addEventListener('click', (event) => {
     logoB.style.display = 'block';
   }
 });
+
+const dwnBtns=document.querySelectorAll('.downArrowBtn');
+const divs=document.querySelectorAll('.mainDiv');
+const divArray=Array.from(divs);
+dwnBtns.forEach((button, index) => {
+  button.addEventListener('click', ()=> {
+    console.log(button);
+      if (index < divArray.length - 1) {
+          divArray[index + 1].scrollIntoView({ behavior: 'smooth' });
+      }
+  });
+});
+const carousel = document.querySelector('.carousel');
+
+const carouselItems = document.querySelectorAll('.carousel_item');
+    let currentIndex = 0;
+    const totalItems = carouselItems.length;
+    const rightButton = document.querySelector('.rightarrow');
+    const leftButton = document.querySelector('.leftarrow');
+
+    rightButton.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % totalItems;
+      carousel.style.transform = `translateX(-${currentIndex * 50}%)`;
+    });
+
+    leftButton.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+      carousel.style.transform = `translateX(-${currentIndex * 50}%)`;
+    });
